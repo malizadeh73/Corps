02c6f44e0d5a0ad38c1ecd3b0e1dcccb1c31b11f
saturn73 <m.alizadeh009@gmail.com>
2017-09-20 01:51:18 +0330

    Mahdi Alizadeh : create home preview project

--- resources/views/user/base/index.blade.php
+++ resources/views/user/base/index.blade.php
@@ -33,16 +33,16 @@
     <nav id="navbar-section" class="navbar navbar-default navbar-static-top navbar-sticky" role="navigation">
         <div class="container">
         
-            <div class="navbar-header navbar-right">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+            {{--<div class="navbar-header navbar-right">--}}
+                {{--<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">--}}
+                    {{--<span class="sr-only">Toggle navigation</span>--}}
+                    {{--<span class="icon-bar"></span>--}}
+                    {{--<span class="icon-bar"></span>--}}
+                    {{--<span class="icon-bar"></span>--}}
+                {{--</button>--}}
 
-                <a class="navbar-brand wow fadeInDownBig" href="index.blade.php"><img class="office-logo" src="{{ asset('assets/img/slider/Office.png') }}" alt="Office"></a>
-            </div>
+                {{--<a class="navbar-brand wow fadeInDownBig" href="index.blade.php"><img class="office-logo" src="{{ asset('assets/img/slider/Office.png') }}" alt="Office"></a>--}}
+            {{--</div>--}}
         
             <div id="navbar-spy" class="collapse navbar-collapse navbar-responsive-collapse">
                 <ul class="nav navbar-nav pull-left">
@@ -70,64 +70,67 @@
 
 
     <!-- Begin #carousel-section -->
-    <section id="carousel-section" class="section-global-wrapper"> 
-        <div class="container-fluid-kamn">
-            <div class="row">
-                <div id="carousel-1" class="carousel slide" data-ride="carousel">
+    {{--<section id="carousel-section" class="section-global-wrapper">--}}
+        {{--<div class="container-fluid-kamn">--}}
+            {{--<div class="row">--}}
+                {{--<div id="carousel-1" class="carousel slide" data-ride="carousel">--}}
 
-                    <!-- Indicators -->
-                    <ol class="carousel-indicators visible-lg">
-                        <li data-target="#carousel-1" data-slide-to="0" class="active"></li>
-                        <li data-target="#carousel-1" data-slide-to="1"></li>
-                        <li data-target="#carousel-1" data-slide-to="2"></li>
-                    </ol>
-        
-                    <!-- Wrapper for slides -->
-                    <div class="carousel-inner" role="listbox">
-                        <!-- Begin Slide 1 -->
-                        <div class="item active">
-                            <img src="{{ asset('assets/img/slider/slide2.jpg') }}" height="400" alt="Image of first carousel">
-                            <div class="carousel-caption">
-                                <h3 class="carousel-title hidden-xs">Office BOOTSTRAP TEMPLATE</h3>
-                                <p class="carousel-body">RESPONSIVE \ MULTI PAGE</p>
-                            </div>
-                        </div>
-                        <!-- End Slide 1 -->
+                    {{--<!-- Indicators -->--}}
+                    {{--<ol class="carousel-indicators visible-lg">--}}
+                        {{--<li data-target="#carousel-1" data-slide-to="0" class="active"></li>--}}
+                        {{--<li data-target="#carousel-1" data-slide-to="1"></li>--}}
+                        {{--<li data-target="#carousel-1" data-slide-to="2"></li>--}}
+                    {{--</ol>--}}
 
-                        <!-- Begin Slide 2 -->
-                        <div class="item">
-                            <img src="{{ asset('assets/img/slider/slide4.jpg') }}" height="400" alt="Image of second carousel">
-                            <div class="carousel-caption">
-                                <h3 class="carousel-title hidden-xs">EASY TO CUSTOMIZE</h3>
-                                <p class="carousel-body">BEAUTIFUL \ CLEAN \ MINIMAL</p>
-                            </div>
-                        </div>
-                        <!-- End Slide 2 -->
+                    {{--<!-- Wrapper for slides -->--}}
+                    {{--<div class="carousel-inner" role="listbox">--}}
+                        {{--<!-- Begin Slide 1 -->--}}
+                        {{--<div class="item active">--}}
+                            {{--<img src="{{ asset('assets/img/slider/slide2.jpg') }}" height="400" alt="Image of first carousel">--}}
+                            {{--<div class="carousel-caption">--}}
+                                {{--<h3 class="carousel-title hidden-xs">Office BOOTSTRAP TEMPLATE</h3>--}}
+                                {{--<p class="carousel-body">RESPONSIVE \ MULTI PAGE</p>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                        {{--<!-- End Slide 1 -->--}}
 
-                        <!-- Begin Slide 3 -->
-                        <div class="item">
-                            <img src="{{ asset('assets/img/slider/slide2.jpg') }}" height="400" alt="Image of third carousel">
-                            <div class="carousel-caption">
-                                <h3 class="carousel-title hidden-xs">MULTI-PURPOSE TEMPLATE</h3>
-                                <p class="carousel-body">PORTFOLIO \ CORPORATE \ CREATIVE</p>
-                            </div>
-                        </div>
-                        <!-- End Slide 3 -->
-                    </div>
-        
-                    <!-- Controls -->
-                    <a class="left carousel-control" href="#carousel-1" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left"></span>
-                    </a>
-                    <a class="right carousel-control" href="#carousel-1" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right"></span>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </section>
+                        {{--<!-- Begin Slide 2 -->--}}
+                        {{--<div class="item">--}}
+                            {{--<img src="{{ asset('assets/img/slider/slide4.jpg') }}" height="400" alt="Image of second carousel">--}}
+                            {{--<div class="carousel-caption">--}}
+                                {{--<h3 class="carousel-title hidden-xs">EASY TO CUSTOMIZE</h3>--}}
+                                {{--<p class="carousel-body">BEAUTIFUL \ CLEAN \ MINIMAL</p>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                        {{--<!-- End Slide 2 -->--}}
+
+                        {{--<!-- Begin Slide 3 -->--}}
+                        {{--<div class="item">--}}
+                            {{--<img src="{{ asset('assets/img/slider/slide2.jpg') }}" height="400" alt="Image of third carousel">--}}
+                            {{--<div class="carousel-caption">--}}
+                                {{--<h3 class="carousel-title hidden-xs">MULTI-PURPOSE TEMPLATE</h3>--}}
+                                {{--<p class="carousel-body">PORTFOLIO \ CORPORATE \ CREATIVE</p>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                        {{--<!-- End Slide 3 -->--}}
+                    {{--</div>--}}
+
+                    {{--<!-- Controls -->--}}
+                    {{--<a class="left carousel-control" href="#carousel-1" data-slide="prev">--}}
+                        {{--<span class="glyphicon glyphicon-chevron-left"></span>--}}
+                    {{--</a>--}}
+                    {{--<a class="right carousel-control" href="#carousel-1" data-slide="next">--}}
+                        {{--<span class="glyphicon glyphicon-chevron-right"></span>--}}
+                    {{--</a>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</section>--}}
     <!-- End #carousel-section -->
 
+    <div class="row container-kamn">
+        <img src="assets/img/slider/heaser.jpg" class="blog-post" alt="Feature-img" align="right" width="100%">
+    </div>
 
     <!-- Begin #services-section -->
     <section id="services" class="services-section section-global-wrapper">
@@ -135,22 +138,22 @@
             <div class="row">
                 <div class="services-header">
                     <h3 class="services-header-title">هدف ما</h3>
-                    <p class="services-header-body"><em> چیز هایی که ما ارائه می دهیم </em>  </p><hr>
+                    <p class="services-header-body"><em> خدماتی که ما ارائه می دهیم </em>  </p><hr>
                 </div>
             </div>
       
             <!-- Begin Services Row 1 -->
             <div class="row services-row services-row-head services-row-1">
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 pull-right">
                     <div class="services-group wow animated fadeInLeft" data-wow-offset="40">
                         <p class="services-icon"><span class="fa fa-android fa-5x"></span></p>
-                        <h4 class="services-title">Android</h4>
-                        <p class="services-body">Cras interdum placerat libero vel tempor. Curabitur gravida iaculis erat quis dignissim.</p>
-                        <p class="services-more"><a href="#">Find Out More</a></p>
+                        <h4 class="services-title">عرصه فرهنگی</h4>
+                        <p class="services-body">یکی از مهم ترین عرصه های فعالیت گروه های جهادی، عرصه فرهنگی است.</p>
+                        <p class="services-more"><a href="#">بیشتر</a></p>
                     </div>
                 </div>
         
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 pull-right">
                     <div class="services-group wow animated zoomIn" data-wow-offset="40">
                         <p class="services-icon"><i class="fa fa-apple fa-5x"></i></p>
                         <h4 class="services-title">Apple</h4>
@@ -159,7 +162,7 @@
                     </div>
                 </div>
         
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 pull-right">
                     <div class="services-group wow animated fadeInRight" data-wow-offset="40">
                         <p class="services-icon"><i class="fa fa-thumbs-o-up fa-5x"></i></p>
                         <h4 class="services-title">EASY TO USE</h4>
@@ -172,7 +175,7 @@
       
             <!-- Begin Services Row 2 -->
             <div class="row services-row services-row-tail services-row-2">
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 pull-right">
                     <div class="services-group wow animated fadeInLeft" data-wow-offset="40">
                         <p class="services-icon"><span class="fa fa-windows fa-5x"></span></p>
                         <h4 class="services-title">Windows</h4>
@@ -181,7 +184,7 @@
                     </div>
                 </div>
         
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 pull-right">
                     <div class="services-group wow animated zoomIn" data-wow-offset="40">
                         <p class="services-icon"><i class="fa fa-eye fa-5x"></i></p>
                         <h4 class="services-title">RETINA READY</h4>
@@ -190,7 +193,7 @@
                     </div>
                 </div>
         
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 pull-right">
                     <div class="services-group wow animated fadeInRight" data-wow-offset="40">
                         <p class="services-icon"><i class="fa fa-cube fa-5x"></i></p>
                         <h4 class="services-title">Cubic</h4>
@@ -223,8 +226,6 @@
                     <p> <a href="#"> درباره ما</a></p>
                     <p> <a href="#"> خدمات</a></p>
                     <p> <a href="#"> تماس با ما</a></p>
-                    <p> <a href="#"> بلاگ</a></p>
-                    <p> <a href="#"> همکاران</a></p> 
                 </div>
               <div class="col-md-4">
                 <h3><i class="fa fa-heart"></i> شبکه های اجتماعی</h3>
